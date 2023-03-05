f01f635d01a7b5d7365cb5fcf083a4507dc0565c
Max Snelling <max.snelling@outlook.com>
2023-03-05 14:45:30 +0000

    feat: add config file and reader methods

---
+++ config_reader.py
@@ -0,0 +1,27 @@
+import yaml
+from utils import cell
+
+CONFIG_PATH = 'config.yaml'
+
+
+def get_file_path() -> str:
+    """
+    Import spreadsheet file path from config file.
+
+    :return: path to spreadsheet file.
+    """
+    with open(CONFIG_PATH, 'r') as file:
+        config = yaml.safe_load(file)
+        return config['source']['file']
+
+
+def get_cells() -> list:
+    """
+    Import cell information from config file.
+
+    :return: List of cells in config file.
+    """
+    with open(CONFIG_PATH, 'r') as file:
+        config = yaml.safe_load(file)
+        cells_input = config['source']['cells']
+        return [cell(i['descriptor'], i['x'], i['y']) for i in cells_input]
---
+++ utils.py
@@ -0,0 +1,6 @@
+class cell:
+
+    def __init__(self, descriptor, x, y):
+        self.descriptor = descriptor
+        self.x = x
+        self.y = y
